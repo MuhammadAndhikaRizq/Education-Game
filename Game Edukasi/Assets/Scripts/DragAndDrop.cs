ece8ab94274e9a2428bc93acda1a645c619c62b1
Muhammad Andhika Rizq <mandhikarizq@gmail.com>
2025-03-23 22:47:48 +0700

    Drag and Drop

---
+++ Game Edukasi/Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    Vector3 offset;
+    Collider2D collider2d;
+    public string destinationTag = "DropArea";
+
+    void Awake()
+    {
+        collider2d = GetComponent<Collider2D>();
+    }
+
+    public void OnMouseDown()
+    {
+        offset = transform.position - MouseWorldPosition();
+    }
+
+    void OnMouseDrag()
+    {
+        transform.position = MouseWorldPosition() + offset;
+    }
+
+    void OnMouseUp() 
+    {
+        collider2d.enabled = false;
+        var rayOrigin = Camera.main.transform.position;
+        var rayDirection = MouseWorldPosition() - Camera.main.transform.position;
+        RaycastHit2D hitInfo;
+        if(hitInfo = Physics2D.Raycast(rayOrigin, rayDirection))
+        {
+            if(hitInfo.transform.tag == destinationTag)
+            {
+                transform.position = hitInfo.transform.position + new Vector3(0, 0, -0.01f);
+            }
+        }
+        collider2d.enabled = true;
+    }
+
+    private Vector3 MouseWorldPosition()
+    {
+        var mouseScreePos = Input.mousePosition;
+        mouseScreePos.z = Camera.main.WorldToScreenPoint(transform.position).z;
+        return Camera.main.ScreenToWorldPoint(mouseScreePos);
+    }
+}
