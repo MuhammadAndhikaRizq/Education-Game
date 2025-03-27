17ed7a184ed4f511c602aa22ad2a1aa9ba9c1e8b
Muhammad Andhika Rizq <mandhikarizq@gmail.com>
2025-03-27 23:27:56 +0700

    Win & Lose Condition Stage 3

--- Game Edukasi/Assets/Scripts/Loop System/ButtonActivetedCondition.cs
+++ Game Edukasi/Assets/Scripts/Loop System/ButtonActivetedCondition.cs
@@ -1,8 +1,10 @@
+using System.Collections;
 using UnityEngine;
 
 public class ButtonActivetedCondition : MonoBehaviour
 {
     public GameObject raiseHand;
+    public GameObject winUI;
     public Transform target;
     public float speed = 5;
 
@@ -19,6 +21,15 @@ public class ButtonActivetedCondition : MonoBehaviour
 
     public void BuutonRaiseHand()
     {
-        raiseHand.transform.localPosition = Vector3.Lerp(transform.position, target.position, Time.deltaTime * speed);
+        raiseHand.transform.localPosition = Vector3.Lerp(raiseHand.transform.position, target.position, Time.deltaTime * speed);
+        StartCoroutine(ActiveWinUI());
+    }
+
+    IEnumerator ActiveWinUI()
+    {
+        yield return new WaitForSeconds(3);
+        raiseHand.SetActive(false);
+        winUI.SetActive(true);
+
     }
 }
