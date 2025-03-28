228e66c5d48feec352b198f331aa7afb3b738648
Muhammad Andhika Rizq <mandhikarizq@gmail.com>
2025-03-28 22:45:25 +0700

    Set Up Layout Stage 4

--- Game Edukasi/Assets/Scripts/CharacterSelector.cs
+++ Game Edukasi/Assets/Scripts/CharacterSelector.cs
@@ -2,38 +2,38 @@ using UnityEngine;
 
 public class CharacterSelector : MonoBehaviour
 {
-    [Header("Character")]
-    public GameObject boy;
-    public GameObject girl;
-     public ActiveCondition characterManager;
+    // [Header("Character")]
+    // public GameObject boy;
+    // public GameObject girl;
+    //  public ActiveCondition characterManager;
 
-    private void Start()
-    {
-        string selectedCharacter = PlayerPrefs.GetString("SelectedCharacter", "Boy");
+    // private void Start()
+    // {
+    //     string selectedCharacter = PlayerPrefs.GetString("SelectedCharacter", "Boy");
 
-        if (selectedCharacter == "Boy")
-        {
-            characterManager.BoyActive(true);
-            characterManager.GirlActive(false);
-        }
-        else
-        {
-            characterManager.BoyActive(false);
-            characterManager.GirlActive(true);
-        }
-    }
+    //     if (selectedCharacter == "Boy")
+    //     {
+    //         characterManager.BoyActive(true);
+    //         characterManager.GirlActive(false);
+    //     }
+    //     else
+    //     {
+    //         characterManager.BoyActive(false);
+    //         characterManager.GirlActive(true);
+    //     }
+    // }
 
-    public void SelectBoy()
-    {
-        characterManager.BoyActive(true);
-        characterManager.GirlActive(false);
-        PlayerPrefs.SetString("SelectedCharacter", "Boy");
-    }
+    // public void SelectBoy()
+    // {
+    //     characterManager.BoyActive(true);
+    //     characterManager.GirlActive(false);
+    //     PlayerPrefs.SetString("SelectedCharacter", "Boy");
+    // }
 
-    public void SelectGirl()
-    {
-        characterManager.BoyActive(false);
-        characterManager.GirlActive(true);
-        PlayerPrefs.SetString("SelectedCharacter", "Girl");
-    }
+    // public void SelectGirl()
+    // {
+    //     characterManager.BoyActive(false);
+    //     characterManager.GirlActive(true);
+    //     PlayerPrefs.SetString("SelectedCharacter", "Girl");
+    // }
 }
