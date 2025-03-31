e5d0a6f521cebb8b4105d26c2ec65e98c3b91542
Muhammad Andhika Rizq <mandhikarizq@gmail.com>
2025-03-31 23:09:53 +0700

    Fixed Character Selector

--- Game Edukasi/Assets/CharacterManager.cs
+++ Game Edukasi/Assets/CharacterManager.cs
@@ -5,22 +5,35 @@ using UnityEngine;
 
 public class CharacterManager : MonoBehaviour
 {
-    public SpriteRenderer sr;
-    public List<Sprite> character  = new List<Sprite>();
+  public List<SpriteRenderer> characterRenderers; // List untuk semua karakter di scene
+    public List<Sprite> characterSprites; // List untuk sprite karakter yang bisa dipilih
     private int selectedChar = 0;
-    public GameObject playerChar;
+
+    void Start()
+    {
+        // Set sprite semua karakter di scene sesuai dengan pilihan terakhir
+        SetAllCharacterSprites();
+    }
 
     public void SelectedCharacterBoy()
     {
         selectedChar = 0;
-        sr.sprite = character[selectedChar];
-        PrefabUtility.SaveAsPrefabAsset(playerChar, "Assets/Prefabs/Character.prefab");
+        SetAllCharacterSprites();
+        PlayerPrefs.SetInt("SelectedCharacter", selectedChar);
     }
 
     public void SelectedCharacterGirl()
     {
         selectedChar = 1;
-        sr.sprite = character[selectedChar];
-        PrefabUtility.SaveAsPrefabAsset(playerChar, "Assets/Prefabs/Character.prefab");
+        SetAllCharacterSprites();
+        PlayerPrefs.SetInt("SelectedCharacter", selectedChar);
+    }
+
+    private void SetAllCharacterSprites()
+    {
+        foreach (SpriteRenderer sr in characterRenderers)
+        {
+            sr.sprite = characterSprites[selectedChar]; // Set semua karakter dengan sprite yang dipilih
+        }
     }
 }
--- Game Edukasi/Assets/Scripts/GameManager.cs
+++ Game Edukasi/Assets/Scripts/GameManager.cs
@@ -1,16 +1,17 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject selectedCharacter;
-    public GameObject player;
-
-    private Sprite playerSprite;
+    public List<SpriteRenderer> characterRenderers; // Semua karakter di scene
+    public List<Sprite> characterSprites; // Sprite karakter yang tersedia
 
     void Start()
     {
-        playerSprite = selectedCharacter.GetComponent<SpriteRenderer>().sprite;
-
-        player.GetComponent<SpriteRenderer>().sprite = playerSprite;
+        int selectedChar = PlayerPrefs.GetInt("SelectedCharacter", 0); // Ambil pilihan karakter terakhir
+        foreach (SpriteRenderer sr in characterRenderers)
+        {
+            sr.sprite = characterSprites[selectedChar];
+        }
     }
 }
